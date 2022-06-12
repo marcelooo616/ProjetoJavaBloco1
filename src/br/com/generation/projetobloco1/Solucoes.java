9807b0d929f8b925abb28516ab82c93a1b16cfd3
marcelooo616 <marcelooo2014@hotmail.com>
2022-06-12 14:56:23 -0300

    subindo atualizações de desing e corrigindo bug

--- src/br/com/generation/projetobloco1/Solucoes.java
+++ src/br/com/generation/projetobloco1/Solucoes.java
@@ -6,18 +6,18 @@ public class Solucoes {
 		
 		String titulo = "-Co2", nome = "marcelo";
 		String[][] meme = {
-				{ "████████████████████████████████████████████████████████████"},
-				{ "█████!!O que voce fazer para compensa toda essa emissão█████"},		
-				{ "████████████████████████████████████████████████████████████"},
-				{ "█████- Deixar os produtos de origem animal █████████████████"},
-				{ "█████ de fora do cardapio por um dia!!██████████████████████"},
-				{ "█████- Utilizar tansporte público███████████████████████████"},
-				{ "█████- utilizar bicicletas para viagens pequenas████████████"},
-				{ "█████- Evitar embalagens plasticas██████████████████████████"},		
-				{ "█████- Fazer o plantio de arvores███████████████████████████"},
-				{ "█████- Incentivo a veículos menos poluentes█████████████████"},
-		        { "█████- Utilize ecobags██████████████████████████████████████"},
-				{ "████████████████████████████████████████████████████████████"}};
+				{ "██████████████████████████████████████████████████████████████████████████████████"},
+				{ "█████!!O que voce fazer para compensa toda essa emissão███████████████████████████"},		
+				{ "██████████████████████████████████████████████████████████████████████████████████"},
+				{ "█████- Deixar os produtos de origem animal ███████████████████████████████████████"},
+				{ "█████ de fora do cardapio por um dia!!████████████████████████████████████████████"},
+				{ "█████- Utilizar tansporte público█████████████████████████████████████████████████"},
+				{ "█████- utilizar bicicletas para viagens pequenas██████████████████████████████████"},
+				{ "█████- Evitar embalagens plasticas████████████████████████████████████████████████"},		
+				{ "█████- Fazer o plantio de arvores█████████████████████████████████████████████████"},
+				{ "█████- Incentivo a veículos menos poluentes███████████████████████████████████████"},
+		        { "█████- Utilize ecobags████████████████████████████████████████████████████████████"},
+				{ "██████████████████████████████████████████████████████████████████████████████████"}};
 
 		for (int l = 0; l < meme.length; l++) {
 			for (int c = 0; c < meme[l].length; c++) {
